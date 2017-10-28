0ffaff7986720f4293a1661658afc8e4ee832f92
Andrew Ly <andyrewly@gmail.com>
2017-10-28 13:44:37 -0700

    Added basic clustering algorithm

--- backend/user.py
+++ backend/user.py
@@ -1,9 +1,10 @@
 class User:
    """A User representation of the class"""
 
-   def __init__(self, user_id, likes, dislikes):
-      self.user_id = user_id
-      self.likes = likes
-      self.dislikes = dislikes
+   def __init__(self, username, activityLikes, foodLikes):
+      self.username = username
+      self.activityLikes = activityLikes
+      self.foodLikes = foodLikes
 
+   
    
--- backend/user_clustering.py
+++ backend/user_clustering.py
@@ -1,10 +1,38 @@
 import json
 
+
+def compareUserActivities(user1, user2):
+   
+   likenessScore = 0
+
+   for like1 in user1.activityLikes:
+      for like2 in user2.activityLikes:
+         if like1 == like2:
+            likenessScore++
+         
+   return likenessScore
+
+def compareUserFoods(user1, user2):
+   
+   likenessScore = 0
+
+   for like1 in user1.foodLikes:
+      for like2 in user2.foodLikes:
+         if like1 == like2:
+            likenessScore++
+         
+   return likenessScore
+
 def createAgglomerateCluster(users):
-  
+   userSimilarities = {}  
+
    for user1 in users:
+      userEntry = {}
       for user2 in users:
-         user1[user2] 
+         userEntry[user2.username] = {'actiity': compareUserActivities(user1, user2), 
+                                      'food': compareUserFoods(user1, user2} 
+      userSimilarities[user1.username] = userEntry
+
 
 def main():
-   
+      
