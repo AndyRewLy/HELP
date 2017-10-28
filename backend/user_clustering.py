30f2706b2c844e6a8a6bad6e6624dce6888b20d7
Andrew Ly <andyrewly@gmail.com>
2017-10-28 15:00:04 -0700

    Added a comparison method to obtain a list of all matching users

--- backend/user_clustering.py
+++ backend/user_clustering.py
@@ -30,16 +30,50 @@ def createAgglomerateCluster(users):
       userEntry = {}
       for user2 in users:
          if user1.username != user2.username:
-            userEntry[user2.username] = {'actiity': compareUserActivities(user1, user2), 
+            userEntry[user2.username] = {'activity': compareUserActivities(user1, user2), 
                                          'food': compareUserFoods(user1, user2)} 
       userSimilarities[user1.username] = userEntry
 
    return userSimilarities
 
+def getHighestComparison(userSimilarities, username):
+
+   matchedActivityUsers = []
+   activityMax = 0
+   matchedFoodUsers = []
+   foodMax = 0
+   highestMatch = userSimilarities[username]
+
+   for key, value in highestMatch.items():
+      activityValue = value['activity']
+
+      if activityValue > activityMax:
+         matchedActivityUsers = [key]
+         activityMax = activityValue
+      elif activityValue == activityMax and activityValue != 0:
+         matchedActivityUsers.append(key)
+
+   for key, value in highestMatch.items():
+      foodValue = value['food']
+
+      if foodValue > foodMax:
+         matchedFoodUsers = [key]
+         foodMax = foodValue
+      elif foodValue == foodMax and foodValue != 0:
+         matchedFoodUsers.append(key)
+
+   return (matchedActivityUsers, matchedFoodUsers)
+
 def main():
    users = [User('chicken', ['chicken'], []),
-            User('chicken1', ['chicken', 'running'], [])]
+            User('chicken1', ['chicken', 'running'], []),
+            User('natashaeatschickens', ['chicken'], [])]
 
-   createAgglomerateCluster(users)
+   user_input = raw_input("Input a user:")
+   userSimilarities = createAgglomerateCluster(users)
+   (activityUsers, foodUsers) = getHighestComparison(userSimilarities, user_input)
+
+   print activityUsers
+   print foodUsers
 
 main()
