5b1a56de9ad9b756006d37029e976372f03496d1
Arjun <arjungup10@gmail.com>
2017-11-05 19:54:00 -0800

    Further abstracted categories.py and added functionality to get default Preferences

--- backend/categories.py
+++ backend/categories.py
@@ -1,5 +1,4 @@
 import json
-from dbConnector import *
 from pprint import pprint
 
 def getJsonFromFile(fileName):
@@ -8,60 +7,68 @@ def getJsonFromFile(fileName):
 
    return data
 
-def categoriesToDB (foodObjects, activeObjects):
-    for x in foodObjects:
-        putFood(x.get("name"), x.get("alias"), x.get("weights"))
-    for x in activeObjects:
-        putActivity(x.get("name"), x.get("alias"), x.get("weights"))
-    return
-
 def getCategories():
-       fileName = "categories.json"
-       jsonData = getJsonFromFile(fileName)
+   fileName = "categories.json"
+   jsonData = getJsonFromFile(fileName)
 
-       activeParents = ["active", "art"]
-       foodParents = ["food", "restaurants"]
+   activeParents = ["active", "art"]
+   foodParents = ["food", "restaurants"]
 
-       activeCategories = []
-       foodCategories = []
+   activeCategories = []
+   foodCategories = []
 
-       for category in jsonData:
-          alias = category["alias"]
-          title = category["title"]
+   for category in jsonData:
+      alias = category["alias"]
+      title = category["title"]
 
-          categoryObject = {"alias": alias, "name": title}
+      categoryObject = {"alias": alias, "name": title}
 
-          parents = category["parents"]
+      parents = category["parents"]
 
-          for parent in parents:
-             if parent in activeParents:
-                activeCategories.insert(0, categoryObject)
-             elif parent in foodParents:
-                foodCategories.insert(0, categoryObject)
+      for parent in parents:
+         if parent in activeParents:
+            activeCategories.insert(0, categoryObject)
+         elif parent in foodParents:
+            foodCategories.insert(0, categoryObject)
 
-       activeObjects = []
-       foodObjects = []
+   return (activeCategories, foodCategories)
 
-       for category1 in activeCategories:
-          alias = category1["alias"]
-          name = category1["name"]
-          relationList = []
+def getCategoryObjects(activeCategories, foodCategories):
+   activeObjects = []
+   foodObjects = []
 
-          for category2 in activeCategories:
-              newRelation = {category2["alias"]: 0, "name": category2["name"]}
-              relationList.insert(0, newRelation)
+   for category1 in activeCategories:
+      alias = category1["alias"]
+      name = category1["name"]
+      relationList = []
 
-          activeObjects.insert(0, {"alias": alias, "name" : name, "weights": relationList})
+      for category2 in activeCategories:
+          newRelation = {category2["alias"]: 0, "name": category2["name"]}
+          relationList.insert(0, newRelation)
 
-       for category1 in foodCategories:
-          alias = category1["alias"]
-          name = category1["name"]
-          relationList = []
+      activeObjects.insert(0, {"alias": alias, "name" : name, "weights": relationList})
 
-          for category2 in foodCategories:
-              newRelation = {category2["alias"]: 0, "name": category2["name"]}
+   for category1 in foodCategories:
+      alias = category1["alias"]
+      name = category1["name"]
+      relationList = []
 
-              relationList.insert(0, newRelation)
+      for category2 in foodCategories:
+          newRelation = {category2["alias"]: 0, "name": category2["name"]}
 
-          foodObjects.insert(0, {"alias": alias, "name": name, "weights": relationList})
-          return (foodObjects, activeObjects)
+          relationList.insert(0, newRelation)
+
+      foodObjects.insert(0, {"alias": alias, "name": name, "weights": relationList})
+
+   return (activeObjects, foodObjects)
+
+def getPreferences(activeCategories, foodCategories):
+   activePreferences = {}
+   foodPreferences = {}
+
+   for activity in activeCategories:
+       activePreferences.update({activity["name"] : 0})
+   for food in foodCategories:
+       foodPreferences.update({food["name"] : 0})
+
+   return (activePreferences, foodPreferences)
